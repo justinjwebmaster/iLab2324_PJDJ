d98aafe5abf036ac7e3333e80ad1ad7ec26b5946
jwebmaster <justin@jwebmaster.be>
2023-11-06 22:57:33 +0100

    Style des filtres, plus ajout boutton fermer pour lancer GET du form

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,10 +1,18 @@
-// Sélectionnez toutes les checkboxes du formulaire
-const checkboxes = document.querySelectorAll('#formFilter input[type="checkbox"]');
-
-// Ajoutez un écouteur d'événement à chaque checkbox
-checkboxes.forEach(function(checkbox) {
-  checkbox.addEventListener('change', function() {
-    // Soumettez le formulaire chaque fois qu'une checkbox est cochée ou décochée
-    document.getElementById('formFilter').submit();
-  });
+// recuper button + filters__form
+const button = document.querySelector('.filters__btn');
+const formFilters = document.querySelector('.filters__form');
+// au clic > afficher filters__form et cache filters__btn
+button.addEventListener('click', function() {
+  formFilters.classList.remove('filters__form--hidden');
+  button.classList.add('filters__btn--hidden');
 });
+
+
+// fermer filters__form et afficher filters__btn
+const buttonClose = document.querySelector('.filters__btn--close');
+buttonClose.addEventListener('click', function() {
+  formFilters.classList.add('filters__form--hidden');
+  button.classList.remove('filters__btn--hidden');
+});
+
+
